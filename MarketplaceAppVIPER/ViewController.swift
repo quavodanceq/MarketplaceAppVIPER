83845980d4e89f6785b6ee5af38b8f6b2775aefe
quavodanceq <kuat.oralbekov1011@mail.ru>
2024-02-25 13:08:50 +0600

    Initial Commit

---
+++ MarketplaceAppVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarketplaceAppVIPER
+//
+//  Created by Куат Оралбеков on 25.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
