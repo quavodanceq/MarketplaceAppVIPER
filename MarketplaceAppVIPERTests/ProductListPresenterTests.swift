046bd2b91aa180d40a8addd011864768e57f99bb
quavodanceq <kuat.oralbekov1011@mail.ru>
2024-03-25 19:10:58 +0500

    product list module unit tests

--- MarketplaceAppVIPERTests/ProductListPresenterTests.swift
+++ MarketplaceAppVIPERTests/ProductListPresenterTests.swift
@@ -9,7 +9,18 @@ import XCTest
 @testable import MarketplaceAppVIPER
 
 final class ProductListPresenterTests: XCTestCase {
-
+    
+    private let interactor = ProductListInteractorMock()
+    private let router = ProductListRouterMock()
+    private let view = ProductListViewMock()
+    private lazy var presenter = ProductsListPresenter(interactor: interactor, router: router)
+    
+    
+    override func setUp() async throws {
+        interactor.pres = presenter
+        presenter.view = view
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,6 +28,8 @@ final class ProductListPresenterTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    
 
     func testExample() throws {
         // This is an example of a functional test case.
@@ -33,23 +46,77 @@ final class ProductListPresenterTests: XCTestCase {
         }
     }
     
-    func testPresentData() {
-        let interactor = ProductListInteractorMock()
-        let router = ProductListRouterMock()
-        let presenter = ProductsListPresenter(interactor: interactor, router: router)
-        XCTAssertNil(nil)
+    func testReceiveData() {
+        let data = [Product(imageName: "", name: "", price: "", imagesNames: [""], sizes: [""]),
+                    Product(imageName: "", name: "", price: "", imagesNames: [""], sizes: [""])]
+        interactor.productsToSend = data
+        presenter.viewDidLoaded()
+        XCTAssertEqual(view.products?.count, 2)
+    }
+    
+    func testReceiveEmptyData() {
+        let data = [Product]()
+        interactor.productsToSend = data
+        presenter.viewDidLoaded()
+        XCTAssertEqual(view.products?.count, 0)
+    }
+    
+    func testReceiveNilData() {
+        interactor.productsToSend = nil
+        presenter.viewDidLoaded()
+        XCTAssertTrue(view.isErrorAlertWasShown)
+    }
+    
+    func testRouting() {
+        let receivedProduct = Product(imageName: "",
+                               name: "",
+                               price: "",
+                               imagesNames: [""],
+                               sizes: [""])
+        presenter.showProductPage(product: receivedProduct)
+        XCTAssertTrue(router.isProductPageVCWasShown)
+        XCTAssertEqual(receivedProduct, router.product)
     }
 
 }
 
 class ProductListInteractorMock: ProductsListInteractorProtocol {
     
-    func fetchProductList() {
-        
+    func fetchProductsList() {
+        pres?.productsListFetched(products: productsToSend)
     }
+    
+    weak var pres: ProductsListPresenterProtocol?
+    
+    var productsToSend: [Product]?
     
 }
 
 class ProductListRouterMock: ProductsListRouterProtocol {
+    
+    var isProductPageVCWasShown: Bool = false
+    
+    var product: Product?
+    
+    func showProductPage(product: Product) {
+        isProductPageVCWasShown = true
+        self.product = product
+    }
+    
+}
+
+class ProductListViewMock: ProductsListViewProtocol {
+    
+    var products: [Product]?
+    
+    var isErrorAlertWasShown: Bool = false
+    
+    func showProducts(products: [Product]) {
+        self.products = products
+    }
+    
+    func showErrorAlert() {
+        isErrorAlertWasShown = true
+    }
     
 }
